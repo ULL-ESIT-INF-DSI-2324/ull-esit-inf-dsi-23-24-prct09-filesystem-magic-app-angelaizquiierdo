d6366a62f2fa9af6ef523a194aa24505bd6b413c
Izquierdo Padrón, Ángela Zhouling <91573735+angelaizquiierdo@users.noreply.github.com>
2024-03-16 21:40:34 +0000

    subir

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = void 0;
+/**
+ * Adds two __numbers__
+ * @param firstNumber Consists of the first operand of the addition
+ * @param secondNumber Consists of the second operand of the addition
+ * @returns The addition of the two numbers `firstNumber` and `secondNumber`
+ * ```typescript
+ * add(1, 7) = 8
+ * ```
+ */
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+exports.add = add;
